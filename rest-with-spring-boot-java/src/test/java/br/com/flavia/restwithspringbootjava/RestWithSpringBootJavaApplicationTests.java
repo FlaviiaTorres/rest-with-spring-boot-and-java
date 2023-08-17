e089381327de21c7f2245eebcd8054b03e55aef8
Flávia Torres <flaviiadev@gmail.com>
2023-08-16 22:01:18 -0300

    fist commit

---
+++ rest-with-spring-boot-java/src/test/java/br/com/flavia/restwithspringbootjava/RestWithSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flavia.restwithspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
